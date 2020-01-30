fbee92acbc3d6ffeec90be0ea9252cee3425d287
Xinkai Wang <546296781@qq.com>
2020-01-30 00:34:22 -0700

    1. Finish the other part of tutorial. 2. Add function to count finish time. 3. Add function to let play use "ESC" to quit game.

--- Roll a ball/Assets/Scripts/PlayerController.cs
+++ Roll a ball/Assets/Scripts/PlayerController.cs
@@ -1,16 +1,39 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using System.Collections;
+using System.Diagnostics;
 
 public class PlayerController : MonoBehaviour
 {
 
     public float speed;
 
+    public Text countText;
+    public Text winText;
+
     private Rigidbody rb;
 
+    private int count;
+    private Stopwatch stw = new Stopwatch();
     void Start()
     {
         rb = GetComponent<Rigidbody>();
+        count = 0;
+        SetCountText();
+        winText.text = "";
+        stw.Start();
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+        #if UNITY_EDITOR
+            UnityEditor.EditorApplication.isPlaying = false;
+        #else
+            Application.Quit();
+        #endif
+        }
     }
 
     void FixedUpdate()
@@ -21,5 +44,25 @@ public class PlayerController : MonoBehaviour
         Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
 
         rb.AddForce(movement * speed);
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Pick Up"))
+        {
+            other.gameObject.SetActive(false);
+            count++;
+            SetCountText();
+        }
+    }
+
+    void SetCountText()
+    {
+        countText.text = "Count: " + count.ToString();
+        if(count == 12)
+        {
+            stw.Stop();
+            winText.text = "You Win!\nFinish Time: " + stw.Elapsed.Seconds.ToString() + "s";
+        }
     }
 }
